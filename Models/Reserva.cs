88e17103eb3ebf1e45c30768af3954a6b4949742
João Rangel <jpar.dev00@gmail.com>
2024-09-12 03:31:32 -0300

    atualização

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,14 +17,16 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= Hospedes.Count)
             {
                 Hospedes = hospedes;
+
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception("Capacidade menor que Número de Hospedes, Lotado");
             }
         }
 
@@ -43,17 +45,20 @@ namespace DesafioProjetoHospedagem.Models
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
+            
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            
 
+            
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = valor -= valor/10;
             }
-
+            
             return valor;
         }
     }
